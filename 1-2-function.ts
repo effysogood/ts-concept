fdbe53b9d8ee2c2a4b8ba50d9b20f50a44b1f8e9
Saeyoung Choi <effysogood@gmail.com>
2024-08-04 14:22:31 +0900

    feat: 1-2 function types

---
+++ 1-2-function.ts
@@ -0,0 +1,29 @@
+{
+  // JavaScript
+  function jsAdd(num1, num2) {
+    return num1 + num2;
+  }
+
+  // TypeScript✨
+  function add(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+
+  // JavaScript 💩
+  function jsFetchNum(id) {
+    // code..
+    // code..
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+
+  // TypeScript✨
+  function fetchNum(id: string): Promise<number> {
+    // code..
+    // code..
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+}
