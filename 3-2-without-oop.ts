3300990f25a60b3e212528ba408a5ad26b3e13e1
Saeyoung Choi <effysogood@gmail.com>
2024-08-09 16:34:44 +0900

    refactor: 커피 만들기 로직 오류 방지 수정

--- 3-2-without-oop.ts
+++ 3-2-without-oop.ts
@@ -5,4 +5,53 @@
     milk: number;
     hasSyrup: boolean;
   }
+
+  type menuList = 'americano' | 'latte' | 'vanilaLatte';
+
+  let beansStock: number = 10;
+  const BEANS_GRAM_PER_SHOT = 20;
+
+  const recipes: Record<string, CoffeeBase> = {
+    americano: {
+      name: 'americano',
+      shots: 2,
+      milk: 0,
+      hasSyrup: false,
+    },
+    latte: {
+      name: 'latte',
+      shots: 1,
+      milk: 2,
+      hasSyrup: false,
+    },
+    vanilaLatte: {
+      name: 'vanilaLatte',
+      shots: 1,
+      milk: 1,
+      hasSyrup: true,
+    },
+  };
+
+  function makeCoffee(order: menuList) {
+    const recipe = recipes[order];
+
+    if (!order) {
+      throw new Error(`Unknown menu: ${order}`);
+    }
+
+    if (beansStock < recipe.shots * BEANS_GRAM_PER_SHOT) {
+      throw new Error(
+        `Out of coffee bean's stock: ${
+          beansStock - recipe.shots * BEANS_GRAM_PER_SHOT
+        }`
+      );
+    }
+
+    beansStock -= recipe.shots * BEANS_GRAM_PER_SHOT;
+    return `Here's your ${order}`;
+  }
+
+  console.log(makeCoffee('americano'));
+  // console.log(makeCoffee('misutgaru'));
+  console.log(beansStock);
 }
