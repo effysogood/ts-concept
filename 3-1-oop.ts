a9a260b52cfc8e3f4960f8e9e0f9b7436da4ba62
Saeyoung Choi <effysogood@gmail.com>
2024-08-09 16:17:59 +0900

    feat: 3-2 객체 지향 컨셉

---
+++ 3-1-oop.ts
@@ -0,0 +1,28 @@
+/**
+ * OOP (Object Oriented Programming)
+ * Encapsulation
+    : 절차 지향 프로그래밍 언어에서는 데이터, 함수가 섞여져 있는 반면,
+    관련있는 데이터와 함수를 한 object로 모으고, 내/외부로 막을 수 있는 캡슐화.
+ * Abstraction
+    : 내부 구조를 알지 못하더라도, 외부 인터페이스에서 동작하도록.
+ * Inheritance (IS-A) 
+    : 상속을 이용하여, 정의해둔 데이터를 재사용
+    (parent, super, base <-> child, sub, derived)
+ * Polymorphism (many + form)
+    : 다형성, animal > makeSound() -> pig, cat, dog
+    다른 객체이지만, 공통 속성을 가지고 있음
+ */
+
+/**
+ * class
+  - template
+  - declare once
+  - no data in
+ */
+
+/**
+   * object
+  - instance of a class
+  - created many times
+  - data in
+   */
