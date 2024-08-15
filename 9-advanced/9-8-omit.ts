725be8896b49b501d13efa55303ad3bae05353f5
Saeyoung Choi <effysogood@gmail.com>
2024-08-15 23:03:27 +0900

    feat: Pick and Omit type

---
+++ 9-advanced/9-8-omit.ts
@@ -0,0 +1,26 @@
+{
+  type Video = {
+    id: string;
+    title: string;
+    url: string;
+    data: string;
+  };
+
+  type VideoMetaData = Omit<Video, 'url' | 'data'>;
+
+  function getVideo(id: string): Video {
+    return {
+      id,
+      title: 'video',
+      url: 'http://video.com',
+      data: 'byte-data',
+    };
+  }
+
+  function getVideoMetaData(id: string): VideoMetaData {
+    return {
+      id,
+      title: 'title',
+    };
+  }
+}
